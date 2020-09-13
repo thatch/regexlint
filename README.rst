53b1052ba74e8f1e9cf6c3cd2da9d7db30fc3dd5
Tim Hatch <tim@timhatch.com>
2020-09-13 11:10:15 -0700

    Add integration test

--- README.rst
+++ README.rst
@@ -2,15 +2,20 @@
 Regexlint
 =========
 
-Regexlint will examine all regular expressions in an importable Pygments
-lexer, and report things that are probably not doing what you think they're
-doing.  For example, patterns like this (which will only match the first)::
+Regexlint is intended to be run as a linter against an importable Pygments
+lexer, and flag problems that will prevent the lexer from performing well (since
+we don't have branch coverage for lexers :)
+
+As a simple example, alternations are first match, so it will flag this as never
+matching "elseif"::
 
     (else|elseif)
 
-It can also warn about a few syntax problems, for example this has two
-problems -- the ``\s+`` outside the groups, and not enough actions in bygroups
-(this one needs two in the args to bygroups)::
+It also understands some Pygments internals, for example ``bygroups(...)```
+needs to have the same number of args as the regex has capture groups.  Too many
+will result in duplicate text; too few will result in missing text.  There
+should also not be any gaps between the capture groups, so this example flags
+two problems::
 
     (r'(foo)\s+(bar)', bygroups(Blah)),
 
@@ -24,7 +29,7 @@ Usage
         or
     regexlint pygments.lexers.web:HtmlLexer
         or
-    python2 regexlint/cmdline.py pygments.lexers.web
+    python3 regexlint/cmdline.py pygments.lexers.web
 
 
 Todo
---
+++ tests/test_integration.py
@@ -0,0 +1,78 @@
+import re
+import subprocess
+import sys
+import tempfile
+import unittest
+from pathlib import Path
+
+STRIP_PATH_RE = re.compile(r"^.*/(?=demo_integration\.py)", re.M)
+
+
+class IntegrationTest(unittest.TestCase):
+    def test_readme_example_one(self):
+        with tempfile.TemporaryDirectory() as d:
+            dp = Path(d)
+            (dp / "demo_integration.py").write_text(
+                """\
+from pygments.lexer import RegexLexer, bygroups
+from pygments.token import Text
+
+class T(RegexLexer):
+    tokens = {
+        "root": [
+            ("(else|elseif)", Text),
+        ],
+    }
+"""
+            )
+
+            proc = subprocess.run(
+                [sys.executable, "-m", "regexlint.cmdline", "demo_integration"],
+                env={"PYTHONPATH": d},
+                encoding="utf-8",
+                stdout=subprocess.PIPE,
+            )
+            output = STRIP_PATH_RE.sub("", proc.stdout)
+
+            self.assertEqual(
+                """\
+demo_integration.py:7: (T:root:pat#1) E105: Potential out of order alternation between 'else' and 'elseif'
+              ("(else|elseif)", Text),
+                      ^ here
+""",
+                output,
+            )
+
+    def test_readme_example_two(self):
+        with tempfile.TemporaryDirectory() as d:
+            dp = Path(d)
+            (dp / "demo_integration.py").write_text(
+                """\
+from pygments.lexer import RegexLexer, bygroups
+from pygments.token import Text
+
+class T(RegexLexer):
+    tokens = {
+        "root": [
+            ("(foo)\\s+(bar)", bygroups(Text, Text)),
+        ],
+    }
+"""
+            )
+
+            proc = subprocess.run(
+                [sys.executable, "-m", "regexlint.cmdline", "demo_integration"],
+                env={"PYTHONPATH": d},
+                encoding="utf-8",
+                stdout=subprocess.PIPE,
+            )
+            output = STRIP_PATH_RE.sub("", proc.stdout)
+
+            self.assertEqual(
+                """\
+demo_integration.py:7: (T:root:pat#1) E108: Gap in capture groups using bygroups
+              ("(foo)\\s+(bar)", bygroups(Text, Text)),
+                     ^ here
+""",
+                output,
+            )
